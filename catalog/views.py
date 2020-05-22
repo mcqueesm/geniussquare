f067a5fbb4a1908732399ba9e145d5eab06877e6
Sean <sean.m.mcqueen@gmail.com>
2020-05-22 15:11:03 -0700

    Fixing module import

--- catalog/views.py
+++ catalog/views.py
@@ -3,8 +3,8 @@ matplotlib.use("Agg")
 import matplotlib.pyplot
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from . import dance.pentomino as pentomino
-from . import dance.dance as dance
+from .dance import pentomino as pentomino
+from .dance import dance as dance
 from .forms import IndexForm
 import numpy as np
 from matplotlib.backends.backend_pdf import PdfPages
