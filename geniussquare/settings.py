b98bc91063284e97661060993de7de72b5a3cc7d
Sean <sean.m.mcqueen@gmail.com>
2020-05-21 18:51:21 -0700

    updated requirements.txt

--- geniussquare/settings.py
+++ geniussquare/settings.py
@@ -131,3 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
