b657d138a9b0a4b057b2dd54e0ab587f76f4b04c
Sean <sean.m.mcqueen@gmail.com>
2020-05-22 18:32:07 -0700

    adjusted whitenoise middleware

--- geniussquare/settings.py
+++ geniussquare/settings.py
@@ -38,13 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog',
-    'catalog.dance',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
