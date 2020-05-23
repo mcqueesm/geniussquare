fb90ef28a874c4354a89b14aa828628194bf8906
Sean <sean.m.mcqueen@gmail.com>
2020-05-22 22:33:27 -0700

    changed allowed hosts

--- geniussquare/settings.py
+++ geniussquare/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gh276xf(!uq%a9c$+*rz%&ie8=e*&wbi$pu25#r!&2-8blj!q='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stormy-escarpment-33099.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
